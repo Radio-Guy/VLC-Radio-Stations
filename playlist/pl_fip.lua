ce319acb49b2e6109076391e328bca92f8b2108f
Radio-Guy <121488605+Radio-Guy@users.noreply.github.com>
2022-12-28 16:19:44 +0000

    Add files via upload

--- playlist/pl_fip.lua
+++ playlist/pl_fip.lua
@@ -1,31 +1,33 @@
 --[[
+ VLC Radio Stations ++ Add-on
+ Various Radio Stations (and their various substations) as VLC Service Discovery addon (lua script):
 
- Various radio stations (SomaFM.com, RadCap.ru, ...) lua script (VLC Playlist parse)
+ SomaFM - https://somafm.com/
+ Rad(io) Cap(rice) - http://www.radcap.ru/
+ FIP (France Inter Paris) - https://www.radiofrance.fr/fip
 
+--- BUGS & REQUESTS: ---
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
-
-
---- BUGS & REQUESTS: ---
 
 Send me a message or open a ticket on github: https://github.com/Radio-Guy
 
 
 --- INSTALLATION ---:
 
-Put the sd_xxx.lua file into the according Service Discovery subfolder of the VLC lua directory, by default:
+Put the sd_xxx.lua file into the according Service Discovery subfolder of the VLC lua directory—by default:
 * Windows (all users): %ProgramFiles%\VideoLAN\VLC\lua\sd\
 * Windows (current user): %APPDATA%\VLC\lua\sd\
-* Linux (all users): /usr/lib/vlc/lua/sd/
+* Linux (all users): /usr/share/vlc/lua/sd/
 * Linux (current user): ~/.local/share/vlc/lua/sd/
 
-Put the pl_xxx.lua file(s) into the according Playlist subfolder of the VLC lua directory, by default:
-* Windows (all users): %ProgramFiles%\VideoLAN\VLC\lua\pl\
-* Windows (current user): %APPDATA%\VLC\lua\pl\
-* Linux (all users): /usr/lib/vlc/lua/pl/
-* Linux (current user): ~/.local/share/vlc/lua/pl/
+Put the pl_xxx.lua files into the according Playlist subfolder of the VLC lua directory—by default:
+* Windows (all users): %ProgramFiles%\VideoLAN\VLC\lua\playlist\
+* Windows (current user): %APPDATA%\VLC\lua\playlist\
+* Linux (all users): /usr/share/vlc/lua/playlist/
+* Linux (current user): ~/.local/share/vlc/lua/playlist/
 
 You will need to place both the sd_xxx.lua as well as the pl_xxx.lua files for this addon to work.
 
@@ -38,7 +40,7 @@ Restart VLC.
 
 * This Service Discovery is available on the left panel of VLC under "Internet" >> ""Radio Stations ++"
 * Each radio station offers several substations, various formats and sometimes several streaming servers.
-* If you are in thumbnail view, you will receive nice and convenient station icons.
+* If you are in thumbnail view, you will receive some nice and convenient station icons. They will however only appear once you entered into the main station. Also, substation thumbnails for SomaFM are buggy and not displayed anymore—if anyone can resolve this bug, please contact me on Github.
 
 --]]
 
